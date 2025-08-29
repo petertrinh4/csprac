6301a2f2906647dfac92b88a6dcd7623c3363060
petertrinh4 <petertrinh04@gmail.com>
2025-08-28 22:12:39 -0400

    Improve homepage images and changed background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,7 +3,7 @@ const Background = () => {
     <div
       className="fixed inset-0 w-full h-full -z-10"
       style={{
-        background: 'linear-gradient(to bottom right, #a1c4fd, #c2e9fb)',
+        background: '#fff',
       }}
     ></div>
   );
